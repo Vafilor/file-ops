2f7d0c949b4c07d1e4103d924a5702df3da28903
Andrey Melnikov <vafilor@gmail.com>
2019-05-24 21:14:11 -0700

    Updated url to github project.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Filesystem operations to index files and hash their contents",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject", # TODO
+    url="https://github.com/Vafilor/file-ops",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
