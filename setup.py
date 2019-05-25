139d8b71684065e1379828a5e8661b9225d1118b
Andrey Melnikov <vafilor@gmail.com>
2019-05-25 09:06:21 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     
 setuptools.setup(
     name="file-ops",
-    version="1.0.3",
+    version="1.0.4",
     author="Andrey Melnikov",
     author_email="vafilor@gmail.com",
     description="Filesystem operations to index files and hash their contents",
